bc695d89e30c2232a3c5d24ecc37a1be5b4373eb
Luis AR <ljarellano84@gmail.com>
2018-03-27 14:00:59 -0300

    Primer Commit

---
+++ LarosBackend/src/main/java/com/laros/LarosBackendApplication.java
@@ -0,0 +1,12 @@
+package com.laros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarosBackendApplication {// implements CommandLineRunner
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarosBackendApplication.class, args);
+	}
+}
---
+++ larosEurekaServer/src/main/java/com/laros/larosEurekaServer/LarosEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.laros.larosEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarosEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarosEurekaServerApplication.class, args);
+	}
+}
