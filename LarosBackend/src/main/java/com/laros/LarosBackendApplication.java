b1759e9b2a032e8be7db379d371f49ea75a5aebd
Luis AR <ljarellano84@gmail.com>
2018-03-27 14:51:23 -0300

    3

--- LarosBackend/src/main/java/com/laros/LarosBackendApplication.java
+++ LarosBackend/src/main/java/com/laros/LarosBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LarosBackendApplication {// implements CommandLineRunner
 
-	public sstatic void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(LarosBackendApplication.class, args);
 	}
 }
